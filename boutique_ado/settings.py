a3cb6afc3b104d522438706511c93a5bbb49f69c
Szymon Czapiewski <simonczedu@gmail.com>
2024-12-09 21:36:57 +0000

    Add home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'home',
 
     ###
     # The following apps are required by allauth:
@@ -65,7 +66,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
